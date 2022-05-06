52d90299e2239b224dba5c616f843c1295c15b5c
Anthony Demott <java152001@gmail.com>
2022-05-06 15:00:13 -0700

    Added Week 15 examples

---
+++ Week 15 Examples/examples/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+
+import Account from './components/example1/Account'
+import Critters from './components/example3/Critters'
+import Search from './components/example2/Search';
+
+function App() {
+    return (
+        // <Account />
+        // <Critters />
+        <Search />
+    )
+}
+
+export default App;
