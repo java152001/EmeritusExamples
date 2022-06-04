86b0ff220179ae21ddf3d713c90db3582823fa4b
Anthony Demott <java152001@gmail.com>
2022-06-03 20:31:13 -0700

    made changes to files

--- Week 5 Examples/examples.js
+++ Week 5 Examples/examples.js
@@ -1,30 +1,34 @@
 /*** Example 1 Slicing ***/
+const arrayExample = [7,5,2,41,3,17];
+const arrayExample2 = [1,3,5,378,183,348,48,4785,6];
 
-// const arrayExample = [7,5,2,41,3,17];
+const sliceElement = (givenArray, splitLength) => {
 
-// const sliceElement = (givenArray, splitLength) => {
+    const givenLength = givenArray.length; // 6
+    const start = givenLength - splitLength; // 6 - 3 = 3
 
-//     const givenLength = givenArray.length;
-//     const start = givenLength - splitLength;
+    return givenArray.slice(start, givenLength);
 
-//     return givenArray.slice(start, givenLength);
-// }
+    // givenArray.slice(3, 6)
+}
 
-// console.log(sliceElement(arrayExample, 3));
+// console.log(sliceElement(arrayExample, 3)); //[41, 3, 17]
+console.log(sliceElement(arrayExample2, 5));
 
 /*** Example 2 Splicing ***/
 
-// const arrayExample2 = [4,18,25,"Otters", true, 3.1415];
+// const arrayExample2 = [4,18,25,"Otters", true, 3.1415, 21, "Giraffes"];
 
 // const spliceElements = (givenArray, spliceLength, thingsToAdd) => {
     
 //     const givenLength = givenArray.length;
-//     const start = givenLength - 1;
+//     const start = givenLength - 1;  // 8 - 1 = 7
 
 //     givenArray.splice(start, spliceLength, thingsToAdd); //Will this work?
 
 //     //If we want to accept an array of extra properties, we could do this
 //     // if (Array.isArray(thingsToAdd)) {
+//     //     //givenArray.concat(thingsToAdd)
 //     //     thingsToAdd.forEach(item => givenArray.push(item));
 //     // } else {
 //     //     givenArray.push(thingsToAdd);
@@ -34,68 +38,76 @@
 
 // }
 
-// console.log(spliceElements(arrayExample2, 1, ["Roger Roger", "It's a trap", false]));
+// console.log(spliceElements(arrayExample2, 1, "Roger Roger"));
 
 /*** Example 3  Splitting ***/
 
 // const stringExample = "NextTech_is_my_favorite thing"
 
 // const superMegaSplitter = (givenString, splitter) => {
-//     return givenString.split(splitter);
+//     const returnArray = givenString.split(splitter);
+
+//     let totalString = "";
+
+//     returnArray.forEach(word => {
+//         totalString += ` ${word}`;
+//     })
+
+//     return totalString;
 // }
 
-// console.log(superMegaSplitter(stringExample, '_'));
-// console.log(superMegaSplitter(stringExample, ' '));
-// console.log(superMegaSplitter(stringExample, 'e'));
+// console.log(superMegaSplitter(stringExample, '_')); //[NextTech, is, my, favorite thing]
+// console.log(superMegaSplitter(stringExample, ' ')); //[NextTech_is_my_favorite, thing]
+// console.log(superMegaSplitter(stringExample, 't')); // [N, xtT, ch_is_my_favorit,  thing]
 
 
 /*** Example 4 ArraySorting and Display ***/
 
-const renderPosts = (boston, container) => {
-    let people = boston.data;
-    // console.log(people);
+// const renderPosts = (boston, container) => {
+//     let people = boston.data;
+//     // console.log(people);
 
-    let html = ``;
+//     let html = ``;
 
-    // people.sort((a, b) => b[11] - a[11]);
-    // people.splice(5);
+//     // people.sort((a, b) => b[11] - a[11]);
+//     // people.splice(5);
 
-    // could also just write:
+//     // could also just write:
 
-    //[ 1, "17439B12-1A55-4B6A-980E-5BAC15841AC4", 1, 1426008064, "525314", 1426008064, "525314", "{\n}", "Adario,Anthony J", "Supvising Claims Agent (Asd)", "ASD Human Resources", "98538.32", "0.00", "1842.87", "0.00", "0.00", "0.00", "0.00", "100381.19", "02132" ]
+//     //[ 1, "17439B12-1A55-4B6A-980E-5BAC15841AC4", 1, 1426008064, "525314", 1426008064, "525314", "{\n}", "Adario,Anthony J", "Supvising Claims Agent (Asd)", "ASD Human Resources", "98538.32", "0.00", "1842.87", "0.00", "0.00", "0.00", "0.00", "100381.19", "02132" ]
 
-    // people.forEach((person, i) => {
-    //     return people[i] = {
-    //         id: person[0],
-    //         salary: person[11],
-    //         name: person[8]
-    //     }
-    // });
+//     // people.forEach((person, i) => {
+//     //     return people[i] = {
+//     //         id: person[0],
+//     //         salary: person[11],
+//     //         name: person[8]
+//     //     }
+//     // });
 
-    console.log(people);
+//     console.log(people);
 
-    people.sort((a, b) => b[11] - a[11]).splice(5);
+//     people.sort((a, b) => b[11] - a[11]).splice(5);
 
-    // console.log(people);
+//     // console.log(people);
 
-    //for part 2
-    people = people.filter(person => person[11] > 200000 );
+//     //for part 2
+//     people = people.filter(person => person[11] > 200000 );
 
-    for (let i = 0; i < people.length; i++) {
-        html +=
-            `<li class="post">
-                <h2>Name: ${people[i][8]}</h2>
-                <h3 id="topSalaries">${people[i][11]}</h3>
-            </li>
-            `
-    }
+//     for (let i = 0; i < people.length; i++) {
+//         html +=
+//             `<li class="post">
+//                 <h2>Name: ${people[i][8]}</h2>
+//                 <h3 id="topSalaries">${people[i][11]}</h3>
+//             </li>
+//             `
+//     }
 
-    let ulCont = document.createElement('ul');
-    ulCont.innerHTML = html;
-    container.appendChild(ulCont);
-}
+//     let ulCont = document.createElement('ul');
+//     ulCont.innerHTML = html;
+//     container.appendChild(ulCont);
+// }
 
-renderPosts(boston, document.getElementById('area'));
+// renderPosts(boston, document.getElementById('area'));
 
 
 
@@ -132,26 +144,26 @@ renderPosts(boston, document.getElementById('area'));
   
 //     container.innerHTML = '<ul id = "topSalaries">' + html + '</ul>';
 //   }
-  // function renderTopEmployees(boston, container) {
-  //   //step 2 solution
-  //   var people = boston.data;
-  //   var topEmployees = people.filter((a) => a[11] > 200000);
-  //   const len = topEmployees.length;
-  //   var html = '';
-  //   for (let i = 0; i < len; i++) {
-  //     html +=
-  //       '<li class="top">' +
-  //       '<h2>' +
-  //       topEmployees[i][8] +
-  //       '</h2>' +
-  //       '<h3>' +
-  //       topEmployees[i][11] +
-  //       '</h3>';
-  //   }
+//   function renderTopEmployees(boston, container) {
+//     //step 2 solution
+//     var people = boston.data;
+//     var topEmployees = people.filter((a) => a[11] > 200000);
+//     const len = topEmployees.length;
+//     var html = '';
+//     for (let i = 0; i < len; i++) {
+//       html +=
+//         '<li class="top">' +
+//         '<h2>' +
+//         topEmployees[i][8] +
+//         '</h2>' +
+//         '<h3>' +
+//         topEmployees[i][11] +
+//         '</h3>';
+//     }
   
-  //   container.innerHTML += '<ul id = "topEmployees">' + html + '</ul>';
-  // }
+//     container.innerHTML += '<ul id = "topEmployees">' + html + '</ul>';
+//   }
   
-  //renderPosts(boston, document.getElementById('container'));
-  //renderTopSalaries(boston, document.getElementById('container'));
-  // renderTopEmployees(boston, document.getElementById('container'));
+//   renderPosts(boston, document.getElementById('area'));
+//   renderTopSalaries(boston, document.getElementById('area'));
+//   renderTopEmployees(boston, document.getElementById('area'));
