85a8d62e8f79ff4730c23cf9592b3c34bea5a20e
Anthony Demott <java152001@gmail.com>
2022-08-26 18:59:32 -0700

    Added in Week 27 examples and starting firebase auth

---
+++ Week 27 Examples/public/balance.js
@@ -0,0 +1,60 @@
+function Balance(){
+  const [show, setShow]     = React.useState(true);
+  const [status, setStatus] = React.useState('');  
+
+  return (
+    <Card
+      bgcolor="info"
+      header="Balance"
+      status={status}
+      body={show ?
+        <BalanceForm setShow={setShow} setStatus={setStatus}/> :
+        <BalanceMsg setShow={setShow}/>}
+    />
+  )
+
+}
+
+function BalanceMsg(props){
+  return(<>
+    <h5>Success</h5>
+    <button type="submit" 
+      className="btn btn-light" 
+      onClick={() => props.setShow(true)}>
+        Check balance again
+    </button>
+  </>);
+}
+
+function BalanceForm(props){
+  const [email, setEmail]   = React.useState('');
+  const [balance, setBalance] = React.useState('');  
+
+  function handle(){
+
+    fetch(`/account/${email}`)
+      .then(response => response.json())
+      .then(data => {
+        setBalance(data.balance);
+        props.setStatus('Your balance is: ' + data.balance);      
+        props.setShow(false);
+      });
+  }
+
+  return (<>
+
+    Email<br/>
+    <input type="input" 
+      className="form-control" 
+      placeholder="Enter email" 
+      value={email} 
+      onChange={e => setEmail(e.currentTarget.value)}/><br/>
+
+    <button type="submit" 
+      className="btn btn-light" 
+      onClick={handle}>
+        Check Balance
+    </button>
+
+  </>);
+}
